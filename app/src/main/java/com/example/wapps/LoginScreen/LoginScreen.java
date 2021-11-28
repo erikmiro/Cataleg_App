ca42080359d4ad9e4f0ea35b7511c18b24b18a99
FatimaSahar <fatimasahar@iesjoandaustria.org>
2021-11-28 23:23:35 +0100

    [WP-26] Changed text fields icon and made it text input layout drawable icon instead of edit text start icon.

--- app/src/main/java/com/example/wapps/LoginScreen/LoginScreen.java
+++ app/src/main/java/com/example/wapps/LoginScreen/LoginScreen.java
@@ -1,10 +1,17 @@
 package com.example.wapps.LoginScreen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.wapps.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginScreen extends AppCompatActivity {
 
@@ -12,5 +19,12 @@ public class LoginScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        // Properties
+        TextInputLayout passwordTxt = findViewById(R.id.loginPasswordTxt);
+
+        //  passwordTxt.setStartIconTintList(ContextCompat.getColorStateList(this, R.color));
+
     }
+
 }
