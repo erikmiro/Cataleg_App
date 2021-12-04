016e48a9771f41b5d45b2ee16b133d5d90e9e3f8
FatimaSahar <fatimasahar@iesjoandaustria.org>
2021-12-04 18:07:33 +0100

    [WP-11] Trying to figure out welcome text animation.

--- app/src/main/java/com/example/wapps/GeneralFragment.java
+++ app/src/main/java/com/example/wapps/GeneralFragment.java
@@ -1,45 +1,25 @@
 package com.example.wapps;
 
+import android.animation.Animator;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.AnimationUtils;
+import android.view.animation.DecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link GeneralFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+import java.util.concurrent.TimeUnit;
+
 public class GeneralFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GeneralFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GeneralFragment newInstance(String param1, String param2) {
-        GeneralFragment fragment = new GeneralFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private int shortAnimationDuration;
+    private TextView welcomeText;
 
     public GeneralFragment() {
         // Required empty public constructor
@@ -49,8 +29,6 @@ public class GeneralFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
@@ -58,6 +36,17 @@ public class GeneralFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_general, container, false);
+        View view = inflater.inflate(R.layout.fragment_general, container, false);
+
+        // Properties
+        welcomeText = view.findViewById(R.id.welcomeGeneralFragment);
+        ImageView image = view.findViewById(R.id.imageView);
+
+        // Instantiating the animation I want to use
+        Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.fade_in);
+        welcomeText.setAnimation(animation);
+       // image.setAnimation(animation);
+
+        return view;
     }
 }
