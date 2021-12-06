b631f4bb07670819b12494dddd6e836ffd7357d7
FatimaSahar <fatimasahar@iesjoandaustria.org>
2021-12-06 22:27:52 +0100

    [WP-25] Added a toolbar, created a lateral menu, trying to add a hamburguer

--- app/src/main/java/com/example/wapps/MainActivity.java
+++ app/src/main/java/com/example/wapps/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.wapps;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,9 +22,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Properties
         Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        DrawerLayout drawerLayout = findViewById(R.id.main_drawer);
+        ActionBarDrawerToggle drawerToggle;
+
+        // Setting
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu_hamburguer);
+
+
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_open, R.string.drawer_close) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                invalidateOptionsMenu();
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                invalidateOptionsMenu();
+            }
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        // Setting item's selection fragment
         bottomNav.setSelectedItemId(R.id.nav_general);
         bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
