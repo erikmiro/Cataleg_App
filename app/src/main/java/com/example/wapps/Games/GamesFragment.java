3a99d2c408dc9ad4e20199148b4ef09bc391ee9a
Erik Michel <erikmichelrobado@gmail.com>
2021-12-25 23:33:33 +0100

    [WP-19] Item layout changed, added data for recycler from database

--- app/src/main/java/com/example/wapps/Games/GamesFragment.java
+++ app/src/main/java/com/example/wapps/Games/GamesFragment.java
@@ -1,21 +1,33 @@
 package com.example.wapps.Games;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.wapps.Games.RecyclerView.PlatformRecyclerViewAdapter;
+import com.example.wapps.Models.Platform;
 import com.example.wapps.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
 public class GamesFragment extends Fragment {
+    private FirebaseFirestore db;
+    ArrayList<Platform> platforms;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,22 +35,31 @@ public class GamesFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_games, container, false);
 
-        ArrayList<String> platforms = new ArrayList<>();
-        platforms.add("Xbox");
-        platforms.add("Playstation");
-        platforms.add("Steam");
-        platforms.add("Epic Games");
-        platforms.add("EA Play");
-        platforms.add("Nintendo");
-        platforms.add("Android");
-        platforms.add("iOS");
-
-
-        RecyclerView recyclerView = root.findViewById(R.id.gamePlatformRecyclerView);
-        PlatformRecyclerViewAdapter adapter = new PlatformRecyclerViewAdapter(platforms);
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-
+        db = FirebaseFirestore.getInstance();
+        platforms = new ArrayList<>();
+        db.collection("GamePlatforms")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()) {
+                            for(QueryDocumentSnapshot document : task.getResult()) {
+                                Platform platform = new Platform();
+                                platform.setName(document.getString("name"));
+                                platform.setImagePath(document.getString("imagePath"));
+                                platform.setHexColor(document.getString("hexColor"));
+                                Log.d("Platform", document.getId() + " => " + document.getData());
+                                platforms.add(platform);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                        RecyclerView recyclerView = root.findViewById(R.id.gamePlatformRecyclerView);
+                        PlatformRecyclerViewAdapter adapter = new PlatformRecyclerViewAdapter(platforms, getContext());
+                        recyclerView.setAdapter(adapter);
+                        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+                    }
+                });
         return root;
     }
 }
--- app/src/main/java/com/example/wapps/Games/RecyclerView/PlatformRecyclerViewAdapter.java
+++ app/src/main/java/com/example/wapps/Games/RecyclerView/PlatformRecyclerViewAdapter.java
@@ -1,22 +1,38 @@
 package com.example.wapps.Games.RecyclerView;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.example.wapps.Models.Platform;
 import com.example.wapps.R;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 public class PlatformRecyclerViewAdapter extends RecyclerView.Adapter<PlatformRecyclerViewAdapter.ViewHolder> {
-    private ArrayList<String> platforms;
+    private ArrayList<Platform> platforms;
+    private Context context;
 
-    public PlatformRecyclerViewAdapter(ArrayList<String> array){
-        platforms = array;
+    public PlatformRecyclerViewAdapter() {
+    }
+
+    public PlatformRecyclerViewAdapter(ArrayList<Platform> platforms, Context context){
+        this.platforms = platforms;
+        this.context = context;
     }
 
     @NonNull
@@ -31,7 +47,18 @@ public class PlatformRecyclerViewAdapter extends RecyclerView.Adapter<PlatformRe
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.platformName.setText(platforms.get(position));
+        // Image loader from firebase using glide (Asks firebase for image hosted url using imagePath to storage)
+        holder.platformBackground.setCardBackgroundColor(Color.parseColor(platforms.get(position).getHexColor()));
+        StorageReference storageReference = FirebaseStorage.getInstance("gs://catrenat-3e277.appspot.com").getReference();
+        storageReference.child(platforms.get(position).getImagePath()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(context) // Context from getContext() in HomeFragment
+                        .load(uri.toString())
+                        .into(holder.platformImage);
+                Log.i("IMAGEGLIDE", uri.toString());
+            }
+        });
     }
 
     @Override
@@ -40,11 +67,13 @@ public class PlatformRecyclerViewAdapter extends RecyclerView.Adapter<PlatformRe
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
-        TextView platformName;
+        ImageView platformImage;
+        CardView platformBackground;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            platformName = itemView.findViewById(R.id.platformName);
+            platformImage = itemView.findViewById(R.id.platformImage);
+            platformBackground = itemView.findViewById(R.id.platformBackground);
         }
     }
 }
