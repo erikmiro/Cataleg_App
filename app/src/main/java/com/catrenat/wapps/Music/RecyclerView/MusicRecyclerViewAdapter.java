4933e109d77d5018d1dfd3fedc9d9003f632d809
FatimaSahar <fatimasahar@iesjoandaustria.org>
2022-01-03 22:02:06 +0100

    [WP-20] Finished music fragment details design.

--- app/src/main/java/com/catrenat/wapps/Music/RecyclerView/MusicRecyclerViewAdapter.java
+++ app/src/main/java/com/catrenat/wapps/Music/RecyclerView/MusicRecyclerViewAdapter.java
@@ -7,8 +7,10 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,15 +23,25 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.catrenat.wapps.Music.MusicDetailsFragment;
 import com.catrenat.wapps.R;
 import com.catrenat.wapps.Models.Music;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.YouTubePlayerListener;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.options.IFramePlayerOptions;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 import com.pierfrancescosoffritti.androidyoutubeplayer.core.ui.DefaultPlayerUiController;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -91,14 +103,8 @@ public class MusicRecyclerViewAdapter extends RecyclerView.Adapter<MusicRecycler
             holder.favouriteImage.setImageResource(current);
         });
 
-        // Adds item object to bundle and sent to Item Details fragments
-        //bundle.putSerializable("Category", category);
-        //dishesListFragment.setArguments(bundle);
-
+        // Music Details Fragment Dialog
         holder.itemView.setOnClickListener(v -> {
-            AppCompatActivity app = (AppCompatActivity) v.getContext();
-            //app.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, musicDetailsFragment, "MusicDetails").addToBackStack(null).commit();
-
             // Create the dialog
             Dialog musicDetailsFragmentPopup = new Dialog(context);
 
@@ -108,20 +114,69 @@ public class MusicRecyclerViewAdapter extends RecyclerView.Adapter<MusicRecycler
             lp.width = WindowManager.LayoutParams.MATCH_PARENT;
             lp.height = WindowManager.LayoutParams.MATCH_PARENT;
 
-            // Show the dialog
+            // Set up the dialog
             musicDetailsFragmentPopup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.argb(100, 0, 0, 0)));
             musicDetailsFragmentPopup.setContentView(R.layout.fragment_music_details);
-
 
             // Elements of the dialog
             View dialogDismissView = musicDetailsFragmentPopup.findViewById(R.id.musicDetailsdismissDialogView);
             ImageView favouriteImage = musicDetailsFragmentPopup.findViewById(R.id.musicDetailFavourite);
+            ImageView songImage = musicDetailsFragmentPopup.findViewById(R.id.songImage);
+            ImageView songImagePlaceholder = musicDetailsFragmentPopup.findViewById(R.id.musicDetailPlaceholderImg);
+            TextView songNameDetails = musicDetailsFragmentPopup.findViewById(R.id.detailsSongName);
+            TextView songArtistDetails = musicDetailsFragmentPopup.findViewById(R.id.detailsSongArtist);
+            ImageView youtubeImage = musicDetailsFragmentPopup.findViewById(R.id.musicDetailsYoutubeImg);
+            TextView youtubeText = musicDetailsFragmentPopup.findViewById(R.id.musicDetailYoutubeText);
+
+            // Set song name and artist
+            songNameDetails.setText(holder.songName.getText());
+            songArtistDetails.setText(holder.songArtist.getText());
+
+            // Image loader from firebase using glide
+            StorageReference storageReference = FirebaseStorage.getInstance("gs://catrenat-3e277.appspot.com").getReference();
+            if(!music.getSongImageUrl().equals("")) {
+                storageReference.child(music.getSongImageUrl()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        songImagePlaceholder.setVisibility(View.INVISIBLE);
+                        Glide.with(context)
+                                .load(uri.toString())
+                                .apply(RequestOptions.bitmapTransform(new RoundedCorners(30)))
+                                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                                .into(songImage);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.i("IMAGE", e.toString());
+                    }
+                });
+            } else {
+                songImage.setImageResource(R.drawable.music_bg_box);
+                songImagePlaceholder.setImageResource(R.drawable.cd);
+                songImagePlaceholder.setVisibility(View.VISIBLE);
+            }
 
             // Change dialog favourite image by touching
             favouriteImage.setOnClickListener(view -> {
                 int current = (!heartPressed) ? R.drawable.ic_music_details_filled_heart : R.drawable.ic_music_details_heart;
                 heartPressed = current != R.drawable.ic_music_details_heart;
                 favouriteImage.setImageResource(current);
+            });
+
+            // Youtube link
+            youtubeImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openYoutube();
+                }
+            });
+
+            youtubeText.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    openYoutube();
+                }
             });
 
             // To be able to dismiss the dialog when touched
@@ -132,12 +187,18 @@ public class MusicRecyclerViewAdapter extends RecyclerView.Adapter<MusicRecycler
                 }
             });
 
+            // Show the dialog
             musicDetailsFragmentPopup.setCancelable(true);
             musicDetailsFragmentPopup.setCanceledOnTouchOutside(true);
             musicDetailsFragmentPopup.show();
             musicDetailsFragmentPopup.getWindow().setAttributes(lp);
 
         });
+    }
+
+    // Open youtube function
+    private void openYoutube() {
+        
     }
 
     // Counting the items in the music list
