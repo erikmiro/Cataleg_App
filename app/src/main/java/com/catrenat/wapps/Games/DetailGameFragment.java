a85ed640e5b38a54a2436243de977590cb45ad8f
Erik Michel <erikmichelrobado@gmail.com>
2022-01-12 00:51:25 +0100

    [WP-17] Created fragment and layout built.

---
+++ app/src/main/java/com/catrenat/wapps/Games/DetailGameFragment.java
@@ -0,0 +1,25 @@
+package com.catrenat.wapps.Games;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.catrenat.wapps.R;
+
+public class DetailGameFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_detail_game, container, false);
+
+
+
+        return root;
+    }
+}
